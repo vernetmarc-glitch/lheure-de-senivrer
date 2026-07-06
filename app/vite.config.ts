31018e8c1f7cc75b5a4b7c692068f5ac262cd1a6
vernetmarc-glitch <vernetmarc@gmail.com>
2026-07-06 13:00:45 +0200

    Mise a jour commentaire URL

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // Base relative pour un déploiement sur GitHub Pages
-// (https://vernetmarc-glitch.github.io/carte-univers-observable/)
+// (https://vernetmarc-glitch.github.io/lheure-de-senivrer/)
 export default defineConfig({
   plugins: [react()],
   base: './',
